09e6d337c1ac71374379e9f75a6207f3133968dd
Dehya Qalbi <dehyafullstack@gmail.com>
2025-06-08 10:20:17 +0700

    feat(floating-circle): custom width, bg color and icon

--- src/components/ui/FloatingCircle.tsx
+++ src/components/ui/FloatingCircle.tsx
@@ -1,6 +1,71 @@
-export default function FloatingCircle() {
-    return (
-      <div className="absolute bottom-[95px] right-[102px] w-[134px] h-[134px] bg-[#2F80ED] rounded-full" />
-    );
-  }
+import { ReactNode } from 'react';
+
+interface FloatingCircleProps {
+  /**
+   * Background color of the circle
+   * @default '#2F80ED'
+   */
+  bgColor?: string;
+  /**
+   * Width of the circle
+   * @default '68px'
+   */
+  width?: string | number;
+  /**
+   * Height of the circle
+   * @default '68px'
+   */
+  height?: string | number;
+  /**
+   * Position from bottom
+   * @default '27px'
+   */
+  bottom?: string | number;
+  /**
+   * Position from right
+   * @default '34px'
+   */
+  right?: string | number;
+  /**
+   * Optional icon to display inside the circle
+   */
+  icon?: ReactNode;
+  /**
+   * Additional CSS classes
+   */
+  className?: string;
+  /**
+   * Click handler
+   */
+  onClick?: () => void;
+}
+
+export default function FloatingCircle({
+  bgColor = '#2F80ED',
+  width = '68px',
+  height = '68px',
+  bottom = '27px',
+  right = '34px',
+  icon,
+  className = '',
+  onClick,
+  ...props
+}: FloatingCircleProps) {
+  return (
+    <div 
+      className={`absolute flex items-center justify-center rounded-full ${className}`}
+      style={{
+        backgroundColor: bgColor,
+        width: typeof width === 'number' ? `${width}px` : width,
+        height: typeof height === 'number' ? `${height}px` : height,
+        bottom: typeof bottom === 'number' ? `${bottom}px` : bottom,
+        right: typeof right === 'number' ? `${right}px` : right,
+      }}
+      onClick={onClick}
+      {...props}
+    >
+      {icon}
+    </div>
+  );
+}
   
